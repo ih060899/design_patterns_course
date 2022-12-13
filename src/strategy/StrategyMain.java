0a36cb9ab2e559d0f771f9763f696520a6f2da16
imran <ih060899@gmail.com>
2022-12-13 20:14:23 +0600

    template

---
+++ src/strategy/StrategyMain.java
@@ -0,0 +1,8 @@
+package strategy;
+
+public class StrategyMain {
+    public static void main(String[] args) {
+        ImageStorage imageStorage = new ImageStorage(new JpegCompressor(), new BlackAndWhiteFilter());
+        imageStorage.store("a");
+    }
+}

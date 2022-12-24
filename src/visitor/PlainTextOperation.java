f7c012fb902d454b3ba9d4584bdba087d0037c73
imran <ih060899@gmail.com>
2022-12-24 20:35:33 +0600

    visitor

---
+++ src/visitor/PlainTextOperation.java
@@ -0,0 +1,13 @@
+package visitor;
+
+public class PlainTextOperation implements Operation {
+    @Override
+    public void apply(HeadingNode heading) {
+        System.out.println("Text-Heading");
+    }
+
+    @Override
+    public void apply(AnchorNode anchor) {
+        System.out.println("Text-Anchor");
+    }
+}
---
+++ src/visitor/VisitorMain.java
@@ -0,0 +1,11 @@
+package visitor;
+
+public class VisitorMain {
+    public static void main(String[] args) {
+        HtmlDocument document = new HtmlDocument();
+        document.add(new HeadingNode());
+        document.add(new AnchorNode());
+        document.execute(new HighlightOperation());
+        document.execute(new PlainTextOperation());
+    }
+}
